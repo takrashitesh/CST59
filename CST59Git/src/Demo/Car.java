7181327f0692dd12fae71f4eee3cddb25c9f2331
takrashitesh <takrashitesh@gmail.com>
2023-12-13 14:30:28 +0530

    added new variable

--- CST59Git/src/Demo/Car.java
+++ CST59Git/src/Demo/Car.java
@@ -3,7 +3,7 @@ package Demo;
 public class Car {
 	int price; 
 	float weight;
-	
+	String name ="nothing";
 	public static void main(String[] args) {
 		Car car = new Car();
 		System.out.println(car.price=100);
